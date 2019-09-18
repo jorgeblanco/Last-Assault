897ebf79c9e73655945de1cbce9b7b381465ef90
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-17 22:43:51 -0700

    Added a basic terrain and a splash screen

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private float loadDelay = 5f;
+    
+    void Awake()
+    {
+        MusicManager[] objs = FindObjectsOfType<MusicManager>();
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    void Start()
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
