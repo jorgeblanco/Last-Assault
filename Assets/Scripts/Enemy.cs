34c0e34660642c88fdba700b81e003f9e5995cb4
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-23 21:29:10 -0700

    Added particle collisions Added a score placeholder with TMP

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionFx;
+    private void OnParticleCollision(GameObject other)
+    {
+        // Assume all particle collisions are bullets
+        Instantiate(explosionFx, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
