129678a56939163d38f8027903b59121dbb3d647
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-23 21:57:39 -0700

    Added a score tracking UI element

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject explosionFx;
+    private Scoreboard _scoreboard;
+
+    private void Start()
+    {
+        _scoreboard = FindObjectOfType<Scoreboard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         // Assume all particle collisions are bullets
         Instantiate(explosionFx, transform.position, Quaternion.identity);
+        _scoreboard.ScoreHit();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    private int _score;
+    private TextMeshProUGUI _scoreText;
+
+    public void ScoreHit(int value = 100)
+    {
+        _score += value;
+        UpdateScore();
+    }
+
+    private void Start()
+    {
+        _scoreText = GetComponent<TextMeshProUGUI>();
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        _scoreText.text = "Score: " + _score;
+    }
+}
